19eeefa2d603c4088bef911f7f8fe081b64eb9e8
RandyYanish <rtaylorfargo@gmail.com>
2023-05-25 13:51:52 -0700

    db setup and server update
    Updated the server.js file to connect to the db. Updated db to include tables for: -department -role -employee.

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'admin',
+        password: 'jener8er',
+        database: 'store_db',
+    },
+    console.log('Connected to the store_db database.')
+);
+
+db.query('')
